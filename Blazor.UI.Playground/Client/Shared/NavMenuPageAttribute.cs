a11cab0628891ebd34ffb1a304f6c8d823a1c7ec
Jens Marchewka <im.naice@googlemail.com>
2023-10-13 14:16:41 +0200

    Initial

---
+++ Blazor.UI.Playground/Client/Shared/NavMenuPageAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Blazor.UI.Playground.Client.Shared
+{
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class NavMenuPageAttribute : Attribute
+    {
+        // This is a positional argument
+        public NavMenuPageAttribute(string text, string iconCSS = "")
+        {
+            Text = text;
+            IconCSS = iconCSS;
+        }
+
+        public string Text { get; private set; }
+        public string IconCSS { get; private set; }
+    }
+}
---
+++ Blazor.UI.Playground/Server/Program.cs
@@ -0,0 +1,45 @@
+using Blazor.UI.Playground.Shared;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+var Summaries = new[] 
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapGet("/api/forecasts", () => 
+    Enumerable.Range(1, 5000).Select(index => new WeatherForecast
+    {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    }))
+    .Produces<IEnumerable<WeatherForecast>>();
+
+app.Run();
